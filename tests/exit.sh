409f959f5512d36f14e2a39e93011865bfd50608
LONGRUYUN <longruyun111@gmail.com>
2016-11-18 22:28:32 -0800

    Create exit.sh

---
+++ tests/exit.sh
@@ -0,0 +1,15 @@
+make all
+echo -e "cd .. || exit\nexit\n" | bin/rshell
+echo 
+echo -e "echo A#exit\nexit\n" | bin/rshell
+echo
+echo -e "exit\n" | bin/rshell
+echo
+echo -e "ls && exit\n" | bin/rshell
+echo
+echo -e "echo A && exit\n" | bin/rshell
+echo
+echo -e "echo A || exit\n" | bin/rshell
+echo
+echo -e "echo A; exit\n" | bin/rshell
+echo
