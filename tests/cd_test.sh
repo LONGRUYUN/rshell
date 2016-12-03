2fb14586dd135bdf966448a0a786e15fc547d2f4
LONGRUYUN <longruyun111@gmail.com>
2016-12-02 22:16:40 -0800

    Create cd_test.sh

---
+++ tests/cd_test.sh
@@ -0,0 +1,10 @@
+make all 
+echo -e "cd src\nexit\n" | bin/rshell
+echo
+echo -e "cd tests\nexit\n" | bin/rshell
+echo
+echo -e "cd bin\nexit\n" | bin/rshell
+echo
+echo -e "cd -\nexit\n" | bin/rshell	#should be syntax error
+echo
+echo -e "cd\nexit\n" | bin/rshell
